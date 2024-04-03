b35c8b7bc319241b9bff26e61b6f3c0c941a9932
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-04-03 15:58:50 -0300

    react hooks form

---
+++ app/pages/index/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import IndexLayout from '@/app/components/layout'
+
+const page = () => {
+  return (
+    <IndexLayout>
+        {null}
+    </IndexLayout>
+
+  )
+}
+
+export default page
