ac8a8e4806aa2fdb9256743b684ac8855d12d58a
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-04-05 02:11:08 -0300

    new home page and update for distribution of folders

--- app/components/homeLayout/homeRoot.tsx
+++ app/components/homeLayout/homeRoot.tsx
@@ -1,14 +1,14 @@
-'use client'
+import Link from 'next/link';
 import '../global.css'
 
-const Home = () => {
+const Home: React.FC = () => {
+
     return (
-        <div className='flex justify-center items-center h-screen'> 
-        <div className='h-3/4 w-2/5 p-4  mx-auto shadow-2xl'> 
+        <div className='flex justify-center items-center h-screen bg-gradient-to-r from-gray-300 to-green-100 '> 
+        <div className='h-3/4 w-2/5 p-4  mx-auto shadow-2xl bg-gray-100'> 
 
                 <div className="text-center">
                     <h1 className="text-6xl md:text-6xl font-bold text-transparent bg-gradient-to-r from-green-400 to-gray-300 bg-clip-text relative inline-block p-4">
-
                         <span id='first' className="inline-block">
                         Hi, welcome to
                         <span id='second' className="text-black italic text-6xl md:text-6xl block">
@@ -22,26 +22,32 @@ const Home = () => {
                     <h2 className='text-green-500 text-2xl md:text-2xl'> Please select one of the options below</h2>
 
                     <div className="w-2/3 sm:w-1/3">
-                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5">
+                        <Link href="../pages/index">
+                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5"
+                        >
                         <span className="text-xl ">
-                        Loggin Account
-                        </span></button>
+                        Register Account
+                        </span></button></Link>
                     </div>
 
              
                     <div className="w-2/3 sm:w-1/3">
-                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5">
+                    <Link href="../pages/loggin">
+                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5"
+                        >
                         <span className="text-xl ">
-                        Register Account
-                        </span></button>
+                        Loggin Account
+                        </span></button></Link>
                     </div>
 
           
                     <div className="w-2/3 sm:w-1/3">
-                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5">
+                    <Link href="../pages/index">
+                        <button className="w-full rounded border-2 border-gray-400 shadow p-2 mb-2 hover:border-green-600  transition-all duration-500 ease-in-out hover:bg-gray-100 hover:p-2.5"
+                        >
                         <span className="text-xl ">
-                        Guest Acces
-                        </span></button>
+                        Guess Access
+                        </span></button></Link>
                     </div>
             </div>
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
           {
             source: '/',
-            destination: '/pages/index',
+            destination: '/pages/home',
             permanent: true,
           },
         ];
